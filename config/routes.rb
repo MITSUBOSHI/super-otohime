ca9221c00d0c1e1b54590f0f7348612b7d8294dd
mitsubosh <yuya.mitsuboshi@gmail.com>
2018-02-04 07:24:28 +0900

    patchあらためput
    close #8

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :songs
-      patch '/room', to: 'room#update'
+      put '/room', to: 'room#update'
     end
   end
 end
